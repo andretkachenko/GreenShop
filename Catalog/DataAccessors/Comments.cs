b7d68f8dee79c97cbf88f73426509baa1e024727
Алексей <alars@i.ua>
2018-12-12 23:45:46 +0200

    CRUD operations for Comments.

---
+++ Catalog/DataAccessors/Comments.cs
@@ -0,0 +1,113 @@
+﻿using Catalog.Utils;
+using Common.Interfaces;
+using Common.Models.Comments;
+using Dapper;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.DataAccessors
+{
+    public class Comments : IDataAccessor<Comment>
+    {
+        private string query;
+
+        public async Task<int> Add(Comment comment)
+        {
+            using (System.Data.SqlClient.SqlConnection context = SqlContext.Context)
+            {
+                query = @"
+                INSERT INTO [Comments]
+                    ([Author],
+                    [Message])
+                VALUES
+                    (@author,
+                    @message)";
+
+
+                int affectedRows = await context.ExecuteAsync(query, new
+                {
+                    author = comment.Author,
+                    message = comment.Message
+                });
+
+                return affectedRows;
+            }
+        }
+
+        public async Task<int> Delete(int id)
+        {
+            using (System.Data.SqlClient.SqlConnection context = SqlContext.Context)
+            {
+                query = @"
+                        DELETE
+                        FROM [Comments]
+                        WHERE [Id] = @id";
+                int affectedRows = await context.ExecuteAsync(query, new
+                {
+                    id
+                });
+                return affectedRows;
+            }
+        }
+
+        public async Task<int> Edit(Comment comment)
+        {
+            using (System.Data.SqlClient.SqlConnection context = SqlContext.Context)
+            {
+                query = @"
+                        UPDATE [Comments]
+                        SET";
+                if (!string.IsNullOrWhiteSpace(comment.Author))
+                {
+                    query += "[Author] = @author";
+                }
+                if (!string.IsNullOrWhiteSpace(comment.Message))
+                {
+                    query += "[Message] = @message";
+                }
+
+                query += " WHERE [Id] = @id";
+                int affectedRows = await context.ExecuteAsync(query, new
+                {
+                    id = comment.Id,
+                    author = comment.Author,
+                    message = comment.Message
+                });
+                return affectedRows;
+            }
+        }
+
+        public async Task<Comment> Get(int id)
+        {
+            using (System.Data.SqlClient.SqlConnection context = SqlContext.Context)
+            {
+                query = @"
+                  SELECT [Id]
+                        ,[Author]
+                        ,[Message]
+                  FROM [Contents]
+                  WHERE [Id] = @id";
+                Comment comment = await context.QueryFirstOrDefaultAsync<Comment>(query, new
+                {
+                    id
+                });
+                return comment;
+            }
+        }
+
+        public async Task<IEnumerable<Comment>> GetAll()
+        {
+            using (System.Data.SqlClient.SqlConnection context = SqlContext.Context)
+            {
+                query = @"
+                  SELECT [Id]
+                        ,[Author]
+                        ,[Message]
+                  FROM [Contents]";
+                IEnumerable<Comment> comments = await context.QueryAsync<Comment>(query);
+
+                return comments;
+            }
+        }
+    }
+}
