4603308c605d0c9f3096b85a80d279c89d38539c
Bledhard <1akaant1@gmail.com>
2019-01-06 15:32:55 +0200

    feat(ApiGateway): Create basic architecture for WebShopping
    Created Empty WebShopping controller with
    necessary dependencies declared.
    Created interfaces for CatalogService and Consumers.
    The idea of IConsumer<T> interface is to consume
    entity specific CRUD API calls.
    These consumers for Categories and Products should
    be created later on and added to the DI Container
    ICatalogService is empty for now. CatalogService
    contains only dependency injection logic.
    Added configuration to access Catalog API. Its url is
    obtained from appsettings, so you should make your
    own appsettings.development.json to start working
    on it.
    ContainerBuilder was renamed to
    ServiceCollectionExtensions and moved to Extensions
    folder, in order to make Startup cleaner, as it is now
    using ServiceCollection extension methods for
    Dependency Injection.

---
+++ ApiGateway/Services/Catalog/Interfaces/IConsumer.cs
@@ -0,0 +1,15 @@
+﻿using Common.Models.Entity.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ApiGateway.Services.Catalog.Interfaces
+{
+    public interface IConsumer<T> where T: IIdentifiable
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> Get(int id);
+        Task<int> Add(T entity);
+        Task<bool> Delete(int id);
+        Task<bool> Edit(T product);
+    }
+}
