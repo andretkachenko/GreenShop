64aad6cc329f96c2c35e3782af9136e66f5cc4ed
Bledhard <1akaant1@gmail.com>
2019-01-13 13:19:45 +0200

    fix(MvcWebApp): Split WebShoppingApiOperations
    As was suggested in code review comment, I've split the
    WebShoppingApiOperations into three separated files.
    Because of that, both UrlsConfig and
    WebShoppingApiOperations became partial classes.

--- MvcWebApp/Config/UrlsConfig.cs
+++ MvcWebApp/Config/UrlsConfig.cs
@@ -1,20 +1,9 @@
 ﻿namespace MvcWebApp.Config
 {
-    public class UrlsConfig
+    public partial class UrlsConfig
     {
-        public class WebShoppingApiOperations
+        public partial class WebShoppingApiOperations
         {
-            public static string GetAllCategories() => $"/api/categories";
-            public static string GetCategory(int id) => $"/api/categories/{id}";
-            public static string AddCategory() => $"/api/categories";
-            public static string DeleteCategory(int id) => $"/api/categories/{id}";
-            public static string EditCategory() => $"/api/categories";
-
-            public static string GetAllProducts() => $"/api/products";
-            public static string GetProduct(int id) => $"/api/products/{id}";
-            public static string AddProduct() => $"/api/products";
-            public static string DeleteProduct(int id) => $"/api/products/{id}";
-            public static string EditProduct() => $"/api/products";
         }
 
         public string WebShoppingApi { get; set; }
