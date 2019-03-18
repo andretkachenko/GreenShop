4ab526e95b01c5eb67f1c1aae9834b0497c931c9
Bledhard <1akaant1@gmail.com>
2019-03-18 23:44:04 +0200

    refactor(mvc): Rewrite MVC controller actions to use ViewModels

---
+++ src/web/MVC/ViewModels/Catalog/ProductsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Common.Models.Products;
+using System.Collections.Generic;
+
+namespace GreenShop.MVC.ViewModels.Catalog
+{
+    public class ProductsViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
