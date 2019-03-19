c9a8627052de7f5deead66b8c1980a3ee0d7d974
alexars <alars0629@gmail.com>
2019-03-19 23:43:53 +0200

    feat(mvc) Comment Api
    Added Comment Api operations string initializers for Comments.
    Added Comment crud methods to CatalogConsumer.

---
+++ src/web/MVC/Config/CommentApiOperations.cs
@@ -0,0 +1,17 @@
+﻿namespace GreenShop.MVC.Config
+{
+    public partial class UrlsConfig
+    {
+        public partial class WebShoppingApiOperations
+        {
+            public class CommentApiOperations
+            {
+                public static string AddComment => $"api/catalog/comments/";
+                public static string GetAllProductComments(int productId) => $"api/catalog/comments/product/{productId}";
+                public static string GetComment(int id) => $"api/catalog/comments/{id}";
+                public static string EditComment(int id) => $"api/catalog/comments/{id}";
+                public static string DeleteComment(int id) => $"api/catalog/comments/{id}";
+            }
+        }
+    }
+}
