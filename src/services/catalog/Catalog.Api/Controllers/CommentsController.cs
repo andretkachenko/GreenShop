1ab107cde7f303ae16ca1584b7ba08823a267dcd
Bledhard <1akaant1@gmail.com>
2019-05-13 22:03:13 +0300

    docs(Catalog): Add comments for CommentsController
    These XML documents are available via Swagger, which
    is now configured to work with xml

--- src/services/catalog/Catalog.Api/Controllers/CommentsController.cs
+++ src/services/catalog/Catalog.Api/Controllers/CommentsController.cs
@@ -1,4 +1,5 @@
-﻿using GreenShop.Catalog.Api.Service.Products;
+﻿using GreenShop.Catalog.Api.Properties;
+using GreenShop.Catalog.Api.Service.Products;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Threading.Tasks;
@@ -18,44 +19,112 @@ namespace GreenShop.Catalog.Api.Controllers
             _productsService = productsService;
         }
 
-        // PUT api/comment/5
+        /// <summary>
+        /// Edit Comment's message
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// 
+        ///     PUT api/comments/5
+        ///     {
+        ///         "Longer longer story"
+        ///     }
+        /// 
+        /// </remarks>
+        /// <param name="id">Id of the Comment which should be changed</param>
+        /// <param name="message">New desired message</param>
+        /// <response code="200">Comment was updated successfully</response>
+        /// <response code="404">Unable to found the Comment</response>  
+        /// <response code="500">Internal Server Error occured while processing the request</response>      
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        [Consumes("application/json")]
         [HttpPut("{id}")]
         public async Task<ActionResult<bool>> EditComment(int id, [FromBody] string message)
         {
             try
             {
                 bool success = await _productsService.EditCommentAsync(id, message);
-                return Ok(success);
+
+                if (success)
+                    return Ok(success);
+                else
+                    return NotFound(string.Format(Resources.FailureResponse, Resources.Update, Resources.Comment, Resources.EntityNotFound));
             }
-            catch (Exception)
+            catch (Exception e)
             {
-                return BadRequest();
+                return StatusCode(500, string.Format(Resources.FailureResponse, Resources.Update, Resources.Comment, e.Message));
             }
         }
 
-        // DELETE api/comment/5
+        /// <summary>
+        /// Delete Comment with the specified Id
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// 
+        ///     DELETE api/comments/5
+        /// 
+        /// </remarks>
+        /// <param name="id">Id for the Comment that should be deleted</param>
+        /// <response code="200">Comment was deleted successfully</response>
+        /// <response code="404">Unable to found the Comment</response>  
+        /// <response code="500">Internal Server Error occured while processing the request</response>       
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
         [HttpDelete("{id}")]
         public async Task<ActionResult<bool>> DeleteCommentAsync(int id)
         {
             try
             {
                 bool success = await _productsService.DeleteCommentAsync(id);
-                return Ok(success);
+
+                if (success)
+                    return Ok(success);
+                else
+                    return NotFound(string.Format(Resources.FailureResponse, Resources.Delete, Resources.Comment, Resources.EntityNotFound));
             }
-            catch (Exception)
+            catch (Exception e)
             {
-                return BadRequest();
+                return StatusCode(500, string.Format(Resources.FailureResponse, Resources.Delete, Resources.Comment, e.Message));
             }
         }
 
-        // POST api/comment
+
+        /// <summary>
+        /// Create a Comment
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// 
+        ///     POST api/comments
+        ///     {
+        ///     "authorId": 1,
+        ///     "message": "Long long story",
+        ///     "productId": 1
+        /// }
+        /// 
+        /// </remarks>
+        /// <param name="comment">Comment with all necessary properties set</param>
+        /// <returns>A newly created Comment</returns>
+        /// <response code="201">Return Id of the newly created Comment</response>
+        /// <response code="400">Unable to successfully process the Comment</response>    
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        [Consumes("application/json")]
         [HttpPost]
         public async Task<ActionResult<int>> AddCommentAsync([FromBody] CommentDto comment)
         {
             try
             {
                 int id = await _productsService.AddCommentAsync(comment);
-                return Ok(id);
+                // CreatedAtAction supplies response with the Location header, which will contain
+                // route to get the Product
+                // For example
+                // Location → .../api/Products/4
+                return CreatedAtAction("GetProductAsync", new { id }, comment.ProductId);
             }
             catch (Exception)
             {
