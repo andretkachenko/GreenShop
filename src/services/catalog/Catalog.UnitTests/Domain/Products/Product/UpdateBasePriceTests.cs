30456648a453d373156dc90f6cdb06b84dfda2ae
Bledhard <1akaant1@gmail.com>
2019-06-13 22:29:07 +0300

    tests(catalog): Add tests for UpdateBasePrice

---
+++ src/services/catalog/Catalog.UnitTests/Domain/Products/Product/UpdateBasePriceTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Target = GreenShop.Catalog.Api.Domain.Products.Product;
+
+namespace GreenShop.UnitTests.Domain.Products.Product
+{
+    [TestClass]
+    public class UpdateBasePriceTests
+    {
+        [TestMethod]
+        public void ValidPrice_SuccessfullyUpdated()
+        {
+            // Assign
+            decimal price = 12m;
+            string initName = "Init Name";
+            int categoryId = 1;
+            Target productMock = new Target(initName, categoryId);
+
+            // Act
+            productMock.UpdateBasePrice(price);
+
+            // Assert
+            Assert.AreEqual(price, productMock.BasePrice);
+        }
+    }
+}
