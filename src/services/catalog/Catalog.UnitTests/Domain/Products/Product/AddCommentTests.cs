ce64d90364d8be3bb214af746e84fa2373218763
Bledhard <1akaant1@gmail.com>
2019-06-17 15:29:11 +0300

    tests(catalog): Add tests for AddCommentTests

---
+++ src/services/catalog/Catalog.UnitTests/Domain/Products/Product/AddCommentTests.cs
@@ -0,0 +1,85 @@
+﻿using GreenShop.Catalog.Api.Domain.Products;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using Target = GreenShop.Catalog.Api.Domain.Products.Product;
+
+namespace GreenShop.UnitTests.Domain.Products.Product
+{
+    [TestClass]
+    public class AddCommentTests
+    {
+        [TestMethod]
+        public void ValidAuthorIdValidMessage_AddedSuccessfully()
+        {
+            // Assign
+            string initName = "Init Name";
+            int initCategoryId = 1;
+            int authorId = 1;
+            string message = "Test message";
+            Target productMock = new Target(initName, initCategoryId);
+
+            // Act
+            productMock.AddComment(authorId, message);
+            Comment actualComment = productMock.Comments.First();
+
+            // Assert
+            Assert.AreEqual(authorId, actualComment.AuthorId);
+            Assert.AreEqual(message, actualComment.Message);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ValidAuthorIdNullMessage_ThrowsArgumentException()
+        {
+            // Assign
+            string initName = "Init Name";
+            int initCategoryId = 1;
+            Target productMock = new Target(initName, initCategoryId);
+            int authorId = 1;
+            string message = null;
+
+            // Act
+            productMock.AddComment(authorId, message);
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ValidAuthorIdEmptyMessage_ThrowsArgumentException()
+        {
+            // Assign
+            string initName = "Init Name";
+            int initCategoryId = 1;
+            Target productMock = new Target(initName, initCategoryId);
+            int authorId = 1;
+            string message = "";
+
+            // Act
+            productMock.AddComment(authorId, message);
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ValidAuthorIdWhitespaceMessage_ThrowsArgumentException()
+        {
+            // Assign
+            string initName = "Init Name";
+            int initCategoryId = 1;
+            Target productMock = new Target(initName, initCategoryId);
+            int authorId = 1;
+            string message = "";
+
+            // Act
+            productMock.AddComment(authorId, message);
+
+            // Assert
+            Assert.Fail();
+        }
+    }
+}
