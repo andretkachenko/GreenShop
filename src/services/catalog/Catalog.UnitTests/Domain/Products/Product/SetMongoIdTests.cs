fa11f93b619c826ab556622983cd5b64416d02b0
Bledhard <1akaant1@gmail.com>
2019-06-18 21:36:04 +0300

    tests(catalog): Add tests for SetMongoId

---
+++ src/services/catalog/Catalog.UnitTests/Domain/Products/Product/SetMongoIdTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Target = GreenShop.Catalog.Api.Domain.Products.Product;
+
+namespace GreenShop.UnitTests.Domain.Products.Product
+{
+    [TestClass]
+    public class SetMongoIdTests
+    {
+        [TestMethod]
+        public void ValidMongoId_UpdatedSuccessfully()
+        {
+            // Assign
+            string initName = "Init Name";
+            int initCategoryId = 1;
+            Target productMock = new Target(initName, initCategoryId);
+            string mongoId = "507f191e810c19729de860ea";
+
+            // Act
+            productMock.SetMongoId(mongoId);
+
+            // Assert
+            Assert.AreEqual(mongoId, productMock.MongoId);
+        }
+    }
+}
