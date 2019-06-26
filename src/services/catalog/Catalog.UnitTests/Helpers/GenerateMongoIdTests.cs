f91631cb0144e146f7fb3380a62d984bff4399fc
Bledhard <1akaant1@gmail.com>
2019-06-26 23:09:26 +0300

    tests(catalog): Add tests for GenerateMongoId()

---
+++ src/services/catalog/Catalog.UnitTests/Helpers/GenerateMongoIdTests.cs
@@ -0,0 +1,24 @@
+﻿using GreenShop.Catalog.Api.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.RegularExpressions;
+
+namespace GreenShop.UnitTests.Helpers
+{
+    [TestClass]
+    public class GenerateMongoIdTests
+    {
+        [TestMethod]
+        public void ReturnsValidBsonId()
+        {
+            // Assign
+            Regex regex = new Regex(@"^[0-9a-fA-F]{24}$");
+
+            // Act
+            string actualId = MongoHelper.GenerateMongoId();
+
+            // Assert
+            bool match = regex.IsMatch(actualId);
+            Assert.IsTrue(match);
+        }
+    }
+}
